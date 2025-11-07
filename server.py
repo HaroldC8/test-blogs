a8d2973b6d181fc0c0a681750ce794e71feb3bfd
HaroldC8 <pogojus.haroldas@gmail.com>
2025-11-08 01:14:19 +0200

    New stuff

--- server.py
+++ server.py
@@ -1,37 +1,45 @@
 from flask import Flask, render_template, url_for
 from waitress import serve
 from markdown import markdown
+from post_urls import POST_URLS
 import json
 import os
 
 SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+SITE_JSON = os.path.join(SITE_ROOT, "static/post_entries/configs")
+SITE_POSTS = os.path.join(SITE_ROOT, "static/post_entries/posts")
 
 app = Flask(__name__)
 
-blogs = [
-    "20251107"
-]
-
-
+post_data = []
+for url in POST_URLS:
+    json_url = os.path.join(SITE_JSON, "config_"+ url  + ".json")
+    json_data = json.load(open(json_url))
+    post_data.append([url, json_data["title"]])
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template(
         "index.html",
-        blog_pages=blogs
+        pages=post_data
     )
 
 @app.route('/post/<path:path>')
 def show_post(path):
+    if(path not in POST_URLS):
+        return "Too bad. Post not found." # TODO
+    
     #html = markdown(infile.read())
-    path = os.path.join(SITE_ROOT, "static/post_entries/configs", "config_20251107.json")
-    data = json.load(open(path))
+    json_url = os.path.join(SITE_JSON, "config_"+ path  + ".json")
+    json_data = json.load(open(json_url))
+    post_url = os.path.join(SITE_POSTS, "post_"+ path  + ".md")
+    post_data = markdown(open(post_url).read())
 
     return render_template(
         "post.html",
-        title="title",
-        content=data
+        title=json_data["title"],
+        content=post_data
     )
 
 if __name__ == "__main__":
